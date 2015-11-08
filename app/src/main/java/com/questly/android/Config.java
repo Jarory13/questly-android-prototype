618f3de4dccab9d12605ab3e684f499010216cdc
Eileen Yau <eileen.yau@venmo.com>
2015-11-07 22:42:10 -0500

    Add config file. Marked TODO to put API keys out of file...

---
+++ app/src/main/java/com/questly/android/Config.java
@@ -0,0 +1,12 @@
+package com.questly.android;
+
+public class Config {
+
+    //TODO: Keeps these API key out of file...
+    
+    public static final String GOOGLE_API_OAUTH ="XXXXXX";
+    public static final String PARSE_APPLICATION_ID="9L8kHmdmS3k0K2G4uWACiHs9aI3lp38SyEfBraFP";
+    public static final String PARSE_CLIENT_ID="Rc25711uUdO8YAWwe80qGLPtGlBiorUOtUAYq9rF";
+    public static final String QUESTLY_FIREBASE_URL="https://questly.quests.firebaseio.com";
+
+}
